3b9dc00d2a93d138fcea59444302f3a40b891b82
jw5374 <60762621+jw5374@users.noreply.github.com>
2022-12-02 22:54:07 -0500

    seemingly first working version, awaiting revision after rust doc chapter 2

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,26 @@
+use rand::{thread_rng, Rng};
 use std::io;
 
+// will be improved with Chapter 2 of rust docs
 fn main() {
-    println!("Enter a number to guess!");
+    let goal_num = thread_rng().gen_range(0..100) as u32;
     let mut guessed = String::new();
-    match io::stdin().read_line(&mut guessed) {
-        Ok(n) => {
-            println!("{n} {guessed}");
+    println!("Enter a number to guess!");
+    let mut num = 101;
+    while num != goal_num {
+        match io::stdin().read_line(&mut guessed) {
+            Ok(_n) => {
+                num = guessed.trim().parse::<u32>().unwrap();
+                println!("{num} {goal_num}");
+                if goal_num < num {
+                    println!("higher")
+                } else {
+                    println!("lower")
+                }
+                guessed = String::new();
+            }
+            Err(error) => println!("{error}"),
         }
-        Err(error) => println!("{error}"),
     }
+    println!("Win!");
 }
